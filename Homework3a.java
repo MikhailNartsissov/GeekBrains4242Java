6491496e8ae749d61c4c3da7e55a87a0656b5925
MikhailNartsissov <126248284+MikhailNartsissov@users.noreply.github.com>
2023-07-10 14:45:30 +0300

    002-Homework3-Algorythms-added

---
+++ Homework3a.java
@@ -0,0 +1,26 @@
+//Необходимо реализовать метод разворота связного списка (двухсвязного или односвязного на выбор).
+
+public class Homework3a {
+    public class SingleDirectionalList {
+        Node head;
+
+
+        public class Node {
+            int value;
+            Node next;
+        }
+
+        public void revert(){
+            if (head != null && head.next != null){
+                revert(head.next, head);
+            }
+        }
+
+        private void revert(Node currentNode, Node previousNode) {
+            if (currentNode.next == null) head =currentNode;
+            else revert(currentNode.next, currentNode);
+            currentNode.next = previousNode;
+            previousNode.next = null;
+        }
+    }
+}
